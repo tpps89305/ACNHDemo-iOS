81cc66806ca32c373afbbd7f6c2e67ef1e0e9ed4
Dispy-Mac <tpps89305@hotmail.com>
2021-07-09 17:34:45 +0800

    Fix the compatibility issue of iOS 12.4

--- ACNHDemo/AppDelegate.swift
+++ ACNHDemo/AppDelegate.swift
@@ -11,7 +11,8 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // Fix the compatibility issue of iOS 12.4
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
