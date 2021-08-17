6932ef29be00bd5a1835003bce71eb31e4044c99
Dispy-Mac <tpps89305@hotmail.com>
2021-08-17 17:57:49 +0800

    Add function to get amount of available fishes, sea creatures, and bugs.

--- ACNHDemo/View/Dashboard/DashboardViewController.swift
+++ ACNHDemo/View/Dashboard/DashboardViewController.swift
@@ -11,18 +11,47 @@ class DashboardViewController: UIViewController {
 
     let viewModel = DashboardVCViewModel()
 
+    @IBOutlet weak var labelAvailableFishes: UILabel!
+    @IBOutlet weak var labelAvailabelSeaCreature: UILabel!
+    @IBOutlet weak var labelAvailabelBugs: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         initViews()
+        bindViewModel()
     }
     
     func initViews() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = viewModel.getTodayInfo()
+        labelAvailableFishes.text = "..."
+        labelAvailabelSeaCreature.text = "..."
+        labelAvailabelBugs.text = "..."
     }
-
+    
+    func bindViewModel() {
+        viewModel.getAvailableFish { countOfFishes in
+            self.labelAvailableFishes.text = String(countOfFishes)
+        }
+        viewModel.getAvailableSeaCreatures { (countOfSeaCreatures) in
+            self.labelAvailabelSeaCreature.text = String(countOfSeaCreatures)
+        }
+        viewModel.getAvailableBugs { (countOfBugs) in
+            self.labelAvailabelBugs.text = String(countOfBugs)
+        }
+    }
+    
+    @IBAction func viewFishPress(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func viewSeaCreaturePress(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func viewBugsPress(_ sender: UITapGestureRecognizer) {
+    }
+    
     /*
     // MARK: - Navigation
 
--- ACNHDemo/ViewModel/Dashboard/DashboardVCViewModel.swift
+++ ACNHDemo/ViewModel/Dashboard/DashboardVCViewModel.swift
@@ -19,5 +19,116 @@ class DashboardVCViewModel {
         print("Dashboard: \(result)")
         return result
     }
+    
+    func getAvailableFish(onCompleted: @escaping (Int) -> Void) {
+        var arrayFishes = Array<Fish>()
+        ACNHProvider.request(.fishes(fishId: 0)) { result in
+            do {
+                let response = try result.get()
+                let fishes = try ACNHJSONDecoder().decode(Fishes.self, from: response.data)
+                for eachKey in fishes.keys {
+                    let eachFish = fishes[eachKey]!
+                    if self.isAvailable(fish: eachFish) {
+                        arrayFishes.append(eachFish)
+                    }
+                }
+                
+                // Sort to get same order array every time(s).
+                arrayFishes.sort { (fish0, fish1) -> Bool in
+                    fish0.fileName < fish1.fileName
+                }
+                onCompleted(arrayFishes.count)
+                print("Done!")
+            } catch {
+                print("Error!")
+            }
+        }
+    }
+    
+    func getAvailableSeaCreatures(onCompleted: @escaping (Int) -> Void) {
+        var arraySeaCreatures = Array<SeaCreature>()
+        ACNHProvider.request(.seaCreatures(seaCreatureId: 0)) { result in
+            do {
+                let response = try result.get()
+                let seaCreatures = try ACNHJSONDecoder().decode(SeaCreatures.self, from: response.data)
+                for eachKey in seaCreatures.keys {
+                    let eachSeaCreature = seaCreatures[eachKey]!
+                    if self.isAvailable(seaCreature: eachSeaCreature) {
+                        arraySeaCreatures.append(seaCreatures[eachKey]!)
+                    }
+                }
+                
+                // Sort to get same order array every time(s).
+                arraySeaCreatures.sort { (seaCreature0, seaCreature1) -> Bool in
+                    seaCreature0.id < seaCreature1.id
+                }
+                onCompleted(arraySeaCreatures.count)
+                print("Done!")
+            } catch {
+                print("Error!")
+            }
+        }
+    }
+    
+    func getAvailableBugs(onCompleted: @escaping (Int) -> Void) {
+        var arrayBugs = Array<Bug>()
+        ACNHProvider.request(.bugs(bugId: 0)) { result in
+            do {
+                let response = try result.get()
+                let bugs = try ACNHJSONDecoder().decode(Bugs.self, from: response.data)
+                for eachKey in bugs.keys {
+                    arrayBugs.append(bugs[eachKey]!)
+                }
+                
+                // Sort to get same order array every time(s).
+                arrayBugs.sort { (bug0, bug1) -> Bool in
+                    bug0.id < bug1.id
+                }
+                onCompleted(arrayBugs.count)
+                print("Done!")
+            } catch {
+                print("Error!")
+            }
+        }
+    }
+    
+    private func isAvailable(fish: Fish) -> Bool {
+        let today = Date()
+        let dateComponents = Calendar.current.dateComponents(in: TimeZone.current, from: today)
+        let month = dateComponents.month!
+        let hour = dateComponents.hour
+        if fish.availability.monthArrayNorthern.contains(month) || fish.availability.isAllYear {
+            if fish.availability.timeArray.contains(hour!) || fish.availability.isAllDay {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func isAvailable(seaCreature: SeaCreature) -> Bool {
+        let today = Date()
+        let dateComponents = Calendar.current.dateComponents(in: TimeZone.current, from: today)
+        let month = dateComponents.month!
+        let hour = dateComponents.hour
+        if seaCreature.availability.monthArrayNorthern.contains(month) || seaCreature.availability.isAllYear {
+            if seaCreature.availability.timeArray.contains(hour!) || seaCreature.availability.isAllDay {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func isAvailable(bug: Bug) -> Bool {
+        let today = Date()
+        let dateComponents = Calendar.current.dateComponents(in: TimeZone.current, from: today)
+        let month = dateComponents.month!
+        let hour = dateComponents.hour
+        if bug.availability.monthArrayNorthern.contains(month) || bug.availability.isAllYear {
+            if bug.availability.timeArray.contains(hour!) || bug.availability.isAllDay {
+                return true
+            }
+        }
+        return false
+    }
 
 }
